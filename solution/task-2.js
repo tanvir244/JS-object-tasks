facd47aa78ede27029e604d5a3fe7980a4ecc7e3
tanvir243 <travelertanvir@gmail.com>
2024-02-01 20:07:43 +0600

    task-2 completed

---
+++ solution/task-2.js
@@ -0,0 +1,17 @@
+// using dot notation
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+car.passengerCapacity = 5;
+console.log(car);
+
+// using square break notation
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+car['passenger capacity'] = 5;
+console.log(car);
